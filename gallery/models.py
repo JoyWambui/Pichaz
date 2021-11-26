0d02a718932e753ef887adeab46825a5adb3444b
JoyWambui <joywambuindegwa@gmail.com>
2021-11-26 14:49:43 +0300

    Create Image model

--- gallery/models.py
+++ gallery/models.py
@@ -7,18 +7,18 @@ class Location(models.Model):
     
     def __str__(self):
         return self.location_name
-# class Category(models.Model):
-#     category_name = models.CharField(max_length=50)
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
     
-#     def __str__(self):
-#         return self.category_name
+    def __str__(self):
+        return self.category_name
 
 class Image(models.Model):
     image_path = models.ImageField(upload_to = 'pictures/')
     image_name = models.CharField(max_length=100)
     description = models.TextField()
     location = models.ForeignKey(Location,on_delete=CASCADE)
-    #categories = models.ManyToManyField(Category)
+    categories = models.ManyToManyField(Category)
     
     def __str__(self):
         return self.image_name
