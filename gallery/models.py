dbbe6e40c573bc566753bd459a28ef4618044c45
JoyWambui <joywambuindegwa@gmail.com>
2021-11-26 14:49:26 +0300

    Create Location model

--- gallery/models.py
+++ gallery/models.py
@@ -1,12 +1,24 @@
 from distutils.command.upload import upload
+from tkinter import CASCADE
 from django.db import models
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.location_name
+# class Category(models.Model):
+#     category_name = models.CharField(max_length=50)
+    
+#     def __str__(self):
+#         return self.category_name
 
 class Image(models.Model):
     image_path = models.ImageField(upload_to = 'pictures/')
     image_name = models.CharField(max_length=100)
     description = models.TextField()
-    # location =
-    # category =
+    location = models.ForeignKey(Location,on_delete=CASCADE)
+    #categories = models.ManyToManyField(Category)
     
     def __str__(self):
         return self.image_name
