689a13b6e9f869d4b29acc0a1e705791aedfdd63
JoyWambui <joywambuindegwa@gmail.com>
2021-11-28 16:27:36 +0300

    Create url for home page

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Image,Location,Category
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('categories',)
+
+admin.site.register(Image,ImageAdmin)
+admin.site.register(Location)
+admin.site.register(Category)
---
+++ gallery/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, re_path
+from . import views
+
+urlpatterns= [
+    path('',views.home, name='Pichaz Homepage'),
+    # re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name='pastNews'),
+    # path('search/', views.search_results, name='search_results'),
+    # path('article/<int:id>', views.article, name='article')
+]
+
+# if settings.DEBUG:
+#      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
