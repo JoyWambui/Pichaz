f2a60723da99faf68b3ab97749d04400c504ccb6
JoyWambui <joywambuindegwa@gmail.com>
2021-11-28 18:15:40 +0300

    Add bootstrap and navbar

--- gallery/views.py
+++ gallery/views.py
@@ -4,6 +4,6 @@ from . models import Image
 
 def index(request):
     '''View function that displays uploaded photos'''
-    # images = Image.get_images()
-    # return render(request, 'index.html',{'images':images})
+    images = Image.get_images()
+    return render(request, 'index.html',{'images':images})
     
