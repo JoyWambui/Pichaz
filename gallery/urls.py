028b6f9e0b7f0f3925c4b5c165aa388432598b93
JoyWambui <joywambuindegwa@gmail.com>
2021-11-28 19:28:39 +0300

    Create view function to display images

--- gallery/urls.py
+++ gallery/urls.py
@@ -10,5 +10,5 @@ urlpatterns= [
     # path('article/<int:id>', views.article, name='article')
 ]
 
-# if settings.DEBUG:
-#      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
